8c32c558dbef48e03985bedfe6d3eaaf6c1d652c
QSVSMITHEQU <smithvila.q@gmail.com>
2023-02-14 13:42:34 +0100

    new-app

---
+++ main.ts
@@ -0,0 +1,7 @@
+// main.tsc
+const saludos = (persona: string) => {
+    return "Hola, " + persona;
+    }
+    const usuario: string = "QT";
+    console.log(usuario);
+    document.body.innerHTML = saludos(usuario);
