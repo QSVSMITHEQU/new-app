9545f27c63ab0c5abb2a4e3c38177966a9c48b04
QSVSMITHEQU <smithvila.q@gmail.com>
2023-02-16 14:43:35 +0100

    angularrrr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Component1Component } from './components/component1/component1.component';
+import { Directive1Directive } from './directives/directive1.directive';
+import { Pipe1Pipe } from './pipes/pipe1.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component,
+    Directive1Directive,
+    Pipe1Pipe
   ],
   imports: [
     BrowserModule
